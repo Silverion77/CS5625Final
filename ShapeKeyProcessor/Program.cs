8a97b5d5ce9910dc85ea37f2128ae80e91db5c0e
Koakuma <icethrush@gmail.com>
2015-05-06 17:14:14 -0400

    work on blend shapes, also smaller version of okuu.dae

---
+++ ShapeKeyProcessor/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Assimp;
+
+namespace ShapeKeyProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] morphNames = new string[] {
+                                      "basis",
+                                      "eyes_closed",
+                                      "smile_eyes",
+                                      "smile_mouth",
+                                      "determined_eyes",
+                                      "brow_down",
+                                      "aa",
+                                      "left_eye_closed",
+                                      "caution",
+                                      "><_eyes",
+                                      "oo",
+                                      "sad_brow"
+                                  };
+
+            List<MorphDisplacement>[] allMorphs = new List<MorphDisplacement>[morphNames.Length];
+            Vector3D[] basis = ShapeKeyImporter.VerticesFromFile("../../frames/okuu_000000.obj");
+
+            List<IDDisplacement>[] allMorphsByID = new List<IDDisplacement>[morphNames.Length];
+
+            Scene baseModel = ShapeKeyImporter.ReadFromFile("../../okuu.dae");
+
+            for (int i = 1; i <= 11; i++)
+            {
+                string num = i.ToString("D6");
+                string morphName = morphNames[i];
+                string filename = "../../frames/okuu_" + num + ".obj";
+                Console.WriteLine("{0} contains {1}", filename, morphName);
+                Vector3D[] morphPos = ShapeKeyImporter.VerticesFromFile(filename);
+                List<MorphDisplacement> morphDiffs = ShapeKeyImporter.differences(basis, morphPos);
+                allMorphs[i] = morphDiffs;
+                allMorphsByID[i] = ShapeKeyImporter.diffMorphsToBase(baseModel, morphDiffs);
+                Console.WriteLine("Matched {0} diffs for morph {1}", allMorphsByID[i].Count, morphName);
+            }
+
+            using (StreamWriter sw = new StreamWriter("../../blend_shapes.txt"))
+            {
+                for (int i = 1; i <= 11; i++)
+                {
+                    string morphName = morphNames[i];
+                    List<IDDisplacement> morphDiffs = allMorphsByID[i];
+                    sw.WriteLine("morph {0}", morphName);
+                    foreach (IDDisplacement md in morphDiffs) {
+                        sw.WriteLine("v {0}", md);
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShapeKeyProcessor/ShapeKeyImporter.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Assimp;
+using Assimp.Configs;
+
+namespace ShapeKeyProcessor
+{
+    struct MorphDisplacement
+    {
+        Vector3D source;
+        Vector3D displacement;
+
+        public Vector3D Vertex
+        {
+            get
+            {
+                return source;
+            }
+        }
+
+        public Vector3D Displacement
+        {
+            get
+            {
+                return displacement;
+            }
+        }
+
+        public MorphDisplacement(Vector3D from, Vector3D diff)
+        {
+            source = from;
+            displacement = diff;
+        }
+
+        public override string ToString()
+        {
+            return Vertex.X + " " + Vertex.Y + " " + Vertex.Z + " " + Displacement.X + " " + Displacement.Y + " " + Displacement.Z;
+        }
+    }
+
+    struct IDDisplacement
+    {
+        public int MeshNum;
+        public int VertID;
+        public Vector3D Displacement;
+
+        public IDDisplacement(int meshNum, int vert, Vector3D disp)
+        {
+            MeshNum = meshNum;
+            VertID = vert;
+            Displacement = disp;
+        }
+
+        public override string ToString()
+        {
+            return MeshNum + " " + VertID + " " + Displacement.X + " " + Displacement.Y + " " + Displacement.Z;
+        }
+    }
+
+    class ShapeKeyImporter
+    {
+        public static Vector3D[] VerticesFromFile(string filename)
+        {
+            List<Vector3D> verts = new List<Vector3D>();
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] pieces = line.Split(' ');
+                    if (!pieces[0].Equals("v")) continue;
+
+                    float x = 0;
+                    float y = 0;
+                    float z = 0;
+                    
+                    bool success = float.TryParse(pieces[1], out x) &&
+                        float.TryParse(pieces[2], out y) &&
+                        float.TryParse(pieces[3], out z);
+
+                    if (!success)
+                    {
+                        throw new FormatException();
+                    }
+
+                    verts.Add(new Vector3D(x, y, z));
+                }
+            }
+            return verts.ToArray();
+        }
+
+        public static List<MorphDisplacement> differences(Vector3D[] basis, Vector3D[] morph)
+        {
+            List<MorphDisplacement> diffs = new List<MorphDisplacement>();
+            for (int i = 0; i < basis.Length; i++)
+            {
+                Vector3D diff = morph[i] - basis[i];
+                if (diff.Length() > 1e-6)
+                {
+                    diffs.Add(new MorphDisplacement(basis[i], diff));
+                }
+            }
+            Console.WriteLine("{0} diffs", diffs.Count);
+            return diffs;
+        }
+
+        static bool findVertexAtPos(Vector3D pos, List<MorphDisplacement> morphDiffs, out MorphDisplacement morph)
+        {
+            foreach (MorphDisplacement md in morphDiffs)
+            {
+                Vector3D diff = md.Vertex - pos;
+                if (diff.Length() < 1e-6)
+                {
+                    morph = md;
+                    return true;
+                }
+            }
+            morph = morphDiffs[0];
+            return false;
+        }
+
+        public static Scene ReadFromFile(string baseModelFile)
+        {
+            Console.WriteLine("Importing {0}", baseModelFile);
+            // Create a new importer
+            AssimpContext importer = new AssimpContext();
+
+            //This is how we add a configuration (each config is its own class)
+            NormalSmoothingAngleConfig config = new NormalSmoothingAngleConfig(66.0f);
+            importer.SetConfig(config);
+
+            //Import the model. All configs are set. The model
+            //is imported, loaded into managed memory. Then the unmanaged memory is released, and everything is reset.
+            Scene model = importer.ImportFile(baseModelFile, PostProcessPreset.TargetRealTimeMaximumQuality);
+
+            importer.Dispose();
+            return model;
+        }
+
+        public static List<IDDisplacement> diffMorphsToBase(Scene model, List<MorphDisplacement> morphDiffs)
+        {
+            List<IDDisplacement> dispsByID = new List<IDDisplacement>();
+
+            int meshNum = 0;
+            foreach (Mesh m in model.Meshes) {
+                int vertID = 0;
+                foreach (Vector3D vert in m.Vertices)
+                {
+                    MorphDisplacement disp;
+                    if (!findVertexAtPos(vert, morphDiffs, out disp))
+                    {
+                        vertID++;
+                        continue;
+                    }
+                    IDDisplacement idd = new IDDisplacement(meshNum, vertID, disp.Displacement);
+                    dispsByID.Add(idd);
+                    vertID++;
+                }
+                meshNum++;
+            }
+
+            return dispsByID;
+        }
+    }
+}
